ecc6a5350c99928b0466956d43369a42e5d46412
Bowie <marketing@letscode.com.br>
2024-06-27 16:45:10 -0300

    add

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 9;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random () * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Você errou :(')
-    if (numeroSecreto > chute) {
-        alert(`O número secreto é maior que ${chute}`)
-    }
+
+// enquanto o chute não for igual ao n.s.
+while (chute !=  numeroSecreto) {
+    chute = prompt (`Escolha um número de 1 a ${numeroMaximo}`);
+     // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+       break;
+    } else { 
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+
+        }
+        tentativas++;
+    } 
 } 
+if (tentativas > 1) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+} else {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+   
+}
+
